f2e8b6d43107e84c06f47347e5cbaef4a310ec22
Hea3veN <hea3venmc@gmail.com>
2015-11-19 19:36:42 -0300

    Add worldgen for cobalt ore

--- src/main/java/com/hea3ven/dulcedeleche/industry/metal/Metal.java
+++ src/main/java/com/hea3ven/dulcedeleche/industry/metal/Metal.java
@@ -8,32 +8,35 @@ import net.minecraftforge.common.util.EnumHelper;
 
 public enum Metal implements IStringSerializable {
 	IRON(ToolMaterial.IRON, ArmorMaterial.IRON, (216 << 16) + (216 << 8) + 216, "oreIron", "blockIron",
-			"ingotIron", "nuggetIron", 0, 64),
+			"ingotIron", "nuggetIron", 0, 64, 20, 9),
 	COPPER(null, null, (210 << 16) + (80 << 8) + 50, "oreCopper", "blockCopper", "ingotCopper",
-			"nuggetCopper", 32, 64),
-	TIN(null, null, (185 << 16) + (210 << 8) + 230, "oreTin", "blockTin", "ingotTin", "nuggetTin", 58, 90),
-	GOLD(ToolMaterial.GOLD, ArmorMaterial.GOLD, 0, "oreGold", "blockGold", "ingotGold", "nuggetGold", 0, 32),
+			"nuggetCopper", 32, 64, 15, 12),
+	TIN(null, null, (185 << 16) + (210 << 8) + 230, "oreTin", "blockTin", "ingotTin", "nuggetTin", 58, 90, 24,
+			7),
+	GOLD(ToolMaterial.GOLD, ArmorMaterial.GOLD, 0, "oreGold", "blockGold", "ingotGold", "nuggetGold", 0, 32,
+			2, 9),
 	BRONZE(EnumHelper.addToolMaterial("bronze", 1, 1024, 6.0F, 2.0F, 16),
 			EnumHelper.addArmorMaterial("bronze", "armor", 15, new int[] {1, 5, 3, 2}, 10),
-			(225 << 16) + (170 << 8) + 80, null, "blockBronze", "ingotBronze", "nuggetBronze", 0, 0),
+			(225 << 16) + (170 << 8) + 80, null, "blockBronze", "ingotBronze", "nuggetBronze", 0, 0, 0, 0),
 	STEEL(EnumHelper.addToolMaterial("steel", 3, 3072, 7.0F, 2.5F, 14),
 			EnumHelper.addArmorMaterial("steel", "armor", 15, new int[] {2, 6, 5, 2}, 10),
-			(100 << 16) + (100 << 8) + 110, null, "blockSteel", "ingotSteel", "nuggetSteel", 0, 0),
+			(100 << 16) + (100 << 8) + 110, null, "blockSteel", "ingotSteel", "nuggetSteel", 0, 0, 0, 0),
 	COBALT(EnumHelper.addToolMaterial("cobalt", 3, 2048, 9.0F, 4.0F, 14),
 			EnumHelper.addArmorMaterial("cobalt", "armor", 15, new int[] {2, 7, 5, 3}, 10),
-			(65 << 16) + (90 << 8) + 205, "oreCobalt", "blockCobalt", "ingotCobalt", "nuggetCobalt", 0, 0),
+			(65 << 16) + (90 << 8) + 205, "oreCobalt", "blockCobalt", "ingotCobalt", "nuggetCobalt", 16, 70,
+			10, 15),
 	FERCO_STEEL(EnumHelper.addToolMaterial("fercoSteel", 3, 4096, 10.0F, 4.5F, 14),
 			EnumHelper.addArmorMaterial("fercoSteel", "armor", 15, new int[] {2, 7, 5, 3}, 10),
 			(50 << 16) + (60 << 8) + 105, null, "blockFercoSteel", "ingotFercoSteel", "nuggetFercoSteel", 0,
-			0),
+			0, 0, 0),
 	TUNGSTEN(EnumHelper.addToolMaterial("tungsten", 3, 3584, 7.0F, 2.5F, 14),
 			EnumHelper.addArmorMaterial("tungsten", "armor", 15, new int[] {3, 8, 6, 3}, 10),
 			(60 << 16) + (50 << 8) + 50, "oreTungsten", "blockTungsten", "ingotTungsten", "nuggetTungsten", 0,
-			0),
+			0, 0, 0),
 	MUSHET_STEEL(EnumHelper.addToolMaterial("mushetSteel", 3, 5120, 8.0F, 3.0F, 14),
 			EnumHelper.addArmorMaterial("mushetSteel", "armor", 15, new int[] {3, 8, 6, 3}, 10),
 			(25 << 16) + (35 << 8) + 25, null, "blockMushetSteel", "ingotMushetSteel", "nuggetMushetSteel", 0,
-			0);
+			0, 0, 0);
 
 	public static Metal get(int index) {
 		return values()[index];
@@ -48,9 +51,12 @@ public enum Metal implements IStringSerializable {
 	private String nuggetName;
 	private int worldMinLevel;
 	private int worldMaxLevel;
+	private int worldCount;
+	private int worldSize;
 
 	private Metal(ToolMaterial toolMaterial, ArmorMaterial armorMaterial, int color, String oreName,
-			String blockName, String ingotName, String nuggetName, int worldMinLevel, int worldMaxLevel) {
+			String blockName, String ingotName, String nuggetName, int worldMinLevel, int worldMaxLevel,
+			int worldCount, int worldSize) {
 		this.toolMaterial = toolMaterial;
 		this.armorMaterial = armorMaterial;
 		this.color = color;
@@ -60,6 +66,8 @@ public enum Metal implements IStringSerializable {
 		this.nuggetName = nuggetName;
 		this.worldMinLevel = worldMinLevel;
 		this.worldMaxLevel = worldMaxLevel;
+		this.worldCount = worldCount;
+		this.worldSize = worldSize;
 	}
 
 	@Override
@@ -81,6 +89,14 @@ public enum Metal implements IStringSerializable {
 
 	public int getWorldMinLevel() {
 		return worldMinLevel;
+	}
+
+	public int getWorldCount() {
+		return worldCount;
+	}
+
+	public int getWorldSize() {
+		return worldSize;
 	}
 
 	public ToolMaterial getToolMaterial() {
--- src/main/java/com/hea3ven/dulcedeleche/industry/world/WorldGeneratorOre.java
+++ src/main/java/com/hea3ven/dulcedeleche/industry/world/WorldGeneratorOre.java
@@ -2,6 +2,8 @@ package com.hea3ven.dulcedeleche.industry.world;
 
 import java.util.Random;
 
+import net.minecraft.block.state.pattern.BlockHelper;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -18,28 +20,24 @@ public class WorldGeneratorOre implements IWorldGenerator {
 	@ObjectHolder("dulcedeleche:ore")
 	public static final BlockMetalOre ore = null;
 	private WorldGenMinable[] minables;
-	private int[] minLevels;
-	private int[] maxLevels;
+	private Metal[] metals;
 
 	@Override
-	public void generate(Random rand, int chunkX, int chunkZ, World world,
-			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+	public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator,
+			IChunkProvider chunkProvider) {
 		if (minables == null) {
-			Metal[] metals = ore.getMetalComponent().getMetals();
+			metals = ore.getMetalComponent().getMetals();
 			minables = new WorldGenMinable[metals.length];
-			minLevels = new int[metals.length];
-			maxLevels = new int[metals.length];
 			for (int i = 0; i < minables.length; i++) {
 				minables[i] = new WorldGenMinable(ore.setMetal(ore.getDefaultState(), metals[i]),
-						9);
-				minLevels[i] = metals[i].getWorldMinLevel();
-				maxLevels[i] = metals[i].getWorldMaxLevel();
+						metals[i].getWorldSize(), (i < 2) ? BlockHelper.forBlock(Blocks.stone) :
+						BlockHelper.forBlock(Blocks.netherrack));
 			}
 		}
 
 		switch (world.provider.getDimensionId()) {
 			case -1:
-				break;
+				generateNether(world, rand, chunkX * 16, chunkZ * 16);
 			case 0:
 				generateSurface(world, rand, chunkX * 16, chunkZ * 16);
 				break;
@@ -50,13 +48,23 @@ public class WorldGeneratorOre implements IWorldGenerator {
 
 	private void generateSurface(World world, Random rand, int x, int z) {
 		for (int i = 0; i < 2; i++) {
-			for (int k = 0; k < 16; k++) {
+			for (int k = 0; k < metals[i].getWorldCount(); k++) {
 				int veinX = x + rand.nextInt(16);
 				int veinZ = z + rand.nextInt(16);
-				int y = minLevels[i] + rand.nextInt(maxLevels[i] - minLevels[i]);
+				int y = metals[i].getWorldMinLevel() + rand.nextInt(
+						metals[i].getWorldMaxLevel() - metals[i].getWorldMinLevel());
 				minables[i].generate(world, rand, new BlockPos(veinX, y, veinZ));
 			}
 		}
 	}
 
+	private void generateNether(World world, Random rand, int x, int z) {
+		for (int k = 0; k < metals[2].getWorldCount(); k++) {
+			int veinX = x + rand.nextInt(16);
+			int veinZ = z + rand.nextInt(16);
+			int y = metals[2].getWorldMinLevel() + rand.nextInt(
+					metals[2].getWorldMaxLevel() - metals[2].getWorldMinLevel());
+			minables[2].generate(world, rand, new BlockPos(veinX, y, veinZ));
+		}
+	}
 }
