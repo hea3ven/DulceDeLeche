d76b051c3e2eab2cb43c0038ef702f81b46083c3
Hea3veN <hea3venmc@gmail.com>
2019-03-19 14:43:46 -0300

    Update to Minecraft 19w11b

--- src/main/kotlin/com/hea3ven/dulcedeleche/modules/food/item/ItemBucketDulceDeLeche.kt
+++ src/main/kotlin/com/hea3ven/dulcedeleche/modules/food/item/ItemBucketDulceDeLeche.kt
@@ -3,19 +3,20 @@ package com.hea3ven.dulcedeleche.modules.food.item
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
-import net.minecraft.item.*
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.minecraft.world.World
 
-class ItemBucketDulceDeLeche :
-        FoodItem(2, 0.2f, false, Item.Settings().stackSize(1).recipeRemainder(Items.BUCKET).itemGroup(ItemGroup.FOOD)) {
-
-    override fun onConsumed(stack: ItemStack, world: World, entity: LivingEntity) {
-        entity.addPotionEffect(StatusEffectInstance(StatusEffects.HASTE, 20 * 20, 1))
-        entity.addPotionEffect(StatusEffectInstance(StatusEffects.SLOWNESS, 35 * 20, 1))
-    }
+class ItemBucketDulceDeLeche(settings: Settings) : Item(settings.stackSize(1).recipeRemainder(Items.BUCKET)) {
 
     override fun onItemFinishedUsing(stack: ItemStack, world: World, entity: LivingEntity): ItemStack? {
         val newStack = super.onItemFinishedUsing(stack, world, entity)
+
+        if (!world.isClient) {
+            entity.addPotionEffect(StatusEffectInstance(StatusEffects.HASTE, 20 * 20, 1))
+            entity.addPotionEffect(StatusEffectInstance(StatusEffects.SLOWNESS, 35 * 20, 1))
+        }
         return if (newStack.isEmpty) ItemStack(Items.BUCKET) else stack
     }
 }
